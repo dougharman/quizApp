5bdd3d07a7945ec5e033bbddb6d4baa1fcc86f04
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-12-26 18:36:22 -0600

    2013_12_26 - Today's work

--- quizApp.js
+++ quizApp.js
@@ -26,114 +26,100 @@ var questions = [
 	];
 
 var currentQuestion = 0;
+var currentQuestionNumber = 1;
+var numberCorrect = 0;
+
 
 $(document).ready(function () {
 	event.preventDefault();
+   
     $(".start").click(function() {
-		$(".question").append("<p>" + questions[0].question + "</p>");
-        $("#name_1").val(questions[0].choices[0]);
-        $("#name_2").val(questions[0].choices[1]);
-        $("#name_3").val(questions[0].choices[2]);
         start();
 	});
-  
-    $(".next").click(function(){
-        nextQuestion();
-    })
     
    $(".start").keydown(function (event) {
         event.preventDefault();
         if (event.which == 13) {
-            $(".question").append("<p>" + questions[0].question + "</p>");
-            $("#name_1").val(questions[0].choices[0]);
-            $("#name_2").val(questions[0].choices[1]);
-            $("#name_3").val(questions[0].choices[2]);
             start();
         }
     });
 
-    $(".next").keydown(function (event) {
-        event.preventDefault();
-        if (event.which == 13) {  
-            nextQuestion();
-        }
-    });
-
-
-
-function start() {
     $("#name_1").click(function () {
             event.preventDefault();
             var guess = 0;
-            checkAnswer(guess);
-            console.log(guess);
-            showAnswer(guess);     
+            checkAnswer(guess); 
         });
 
     $("#name_2").click(function () {
             event.preventDefault();
             var guess = 1;
-            checkAnswer(guess);
-            console.log(guess);
-            showAnswer(guess);      
+            checkAnswer(guess);  
         });
 
     $("#name_3").click(function () {
             event.preventDefault();
             var guess = 2;
             checkAnswer(guess);
-            console.log(guess);
-            showAnswer(guess);
         });
+
+    $(".re_start").click(function() {
+        var currentQuestion = 0;  
+        var currentQuestionNumber = 1;
+        numberCorrect = 0;
+      
+        start();
+        });
+
+function start() {
+    event.preventDefault();
+
+    
+
+    $(".question").empty();
+    $(".question").append("<p>" + questions[currentQuestion].question + "</p> ");
+    $("#name_1").val(questions[currentQuestion].choices[0]);
+    $("#name_2").val(questions[currentQuestion].choices[1]);
+    $("#name_3").val(questions[currentQuestion].choices[2]);
+
+    
 }
 
-
 function checkAnswer(guess) {
-    var currentQuestion = 0;
+    
     var correctAnswer = questions[currentQuestion].choices[questions[currentQuestion].correct];
-   
+    console.log(numberCorrect);
     console.log(guess);
     console.log(questions[currentQuestion].correct);
     console.log(correctAnswer);
 
     if (guess === questions[currentQuestion].correct) {                                      
-       $(".response").append("<p>Correct, your current score is xxxx<p>");
-       console.log("Correct");
+        numberCorrect++
+        $(".response").empty();
+        $(".response").append("<p> Correct! <p>");
+    } else if (guess !== questions[currentQuestion].correct) {
+        $(".response").empty();
+        $(".response").append("<p> Sorry, the correct answer is " + correctAnswer + "</p>");
     }
-    else if (guess !== questions[currentQuestion].correct) {
-        $(".response").append("<p>Sorry, the correct answer is " + correctAnswer + "</p>");
-        console.log("Sorry");
-    }
-}
-
-function showAnswer(guess) {
-    var currentQuestion = 0;
-    var numberCorrect = [];
-    if (guess === questions[currentQuestion].correct) {                                      
-       numberCorrect.push(guess);
-    } 
     
-    console.log(numberCorrect.length);
-
-    var currentQuestionNumber = (currentQuestion + 1);
-    console.log(currentQuestionNumber);
-
-    $(".numberCorrect").append("<p>" + numberCorrect.length + "<p>");
-    $("questionNumber").append("<p>" + questions[currentQuestion] + "<p>");
+    $(".numberCorrect").empty();
+    $(".numberCorrect").append("<p>You've answered " + numberCorrect + " of "  + currentQuestionNumber + " correctly.<p>");
+    currentQuestion++
+    currentQuestionNumber++
+       
+    
+       
+    next();   
+   
 }
 
-function nextQuestion() {
-    for ( var i = 0; i < questions.length; i++ ) {
-        currentQuestion++;
-        }   
-    $(".question").contents().remove();
-    $(".question").append("<p>" + questions[currentQuestion].question + "</p>");
-    $("#name_1").val(questions[currentQuestion].choices[0]);
-    $("#name_2").val(questions[currentQuestion].choices[1]);
-    $("#name_3").val(questions[currentQuestion].choices[2]);
-    start();   
+function next() {
+    $(".next").click(function() {
+        $(".response").empty();
+        start();
+    });
 }
 
 });
+
 
 
